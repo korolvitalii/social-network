daff25375ddc7c243f9f575ff2bebe892ddb4344
korolvitalii <korolvitalik@gmail.com>
2021-11-08 08:27:31 +0100

    change api-key

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-  headers: { 'API-KEY': '2a2cd6c3-7d75-445b-87fa-e960094105e7' },
+  headers: { 'API-KEY': 'f922abd5-a253-4933-afca-4c7af432e585' },
 });
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
